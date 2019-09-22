211910dd72579583f41e226c784a532d6cc561de
Johnpeace <princeolatjohnjoe@yahoo.com>
2019-09-22 10:07:52 +0100

    hotfixing heroku

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.get('/api/v1', (req, res) => {
   res.status(200).json({
